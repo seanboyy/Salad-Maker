bd258ac0536da6541d16a55bd9264b6794b3d842
Seanboyy <sbamford12@gmail.com>
2020-07-16 00:56:16 -0400

    Added dummy logic for grabbing and dropping with both characters

---
+++ Assets/AssetLibrary/Scripts/Player/PlayerGrab.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrab : MonoBehaviour
+{
+    public int playerNumber = 0;
+    public bool isHolding = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //Grabbing should only be done when an object to grab is near, and dropping should only be done when a place to drop an item is near
+    //I.E. cannot grab air, cannot drop onto floor
+    void Update()
+    {
+        if (playerNumber == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                isHolding = !isHolding;
+                if (isHolding) Debug.Log("Player One Grabbed Something!");
+                else Debug.Log("Player One Dropped Something!");
+            }
+        }
+        else if (playerNumber == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.RightControl))
+            {
+                isHolding = !isHolding;
+                if (isHolding) Debug.Log("Player Two Grabbed Something!");
+                else Debug.Log("Player Two Dropped Something!");
+            }
+        }
+    }
+}
