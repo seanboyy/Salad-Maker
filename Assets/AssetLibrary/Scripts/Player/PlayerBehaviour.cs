37dba0123939ac9ca530b66e688ba1beee74fbcd
Seanboyy <sbamford12@gmail.com>
2020-07-16 15:11:45 -0400

    Merged PlayerGrab and PlayerMove into one file, added player and game constants file which will be extended in future

---
+++ Assets/AssetLibrary/Scripts/Player/PlayerBehaviour.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private CharacterController controller;
+
+    [Header("Player Attribues")]
+    public float speed = 10.0F;
+    public int playerNumber = 0;
+
+    public Queue<Vegetable> heldItems = new Queue<Vegetable>(2);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = 0F, deltaY = 0F;
+        #region Movement
+        if (playerNumber == 1)
+        {
+            if (Input.GetKey(PlayerConstants.ControlsDict[PlayerConstants.PlayerControls.Player1Left]))
+            {
+                deltaX = -1 * speed * Time.deltaTime;
+            }
+            else if (Input.GetKey(PlayerConstants.ControlsDict[PlayerConstants.PlayerControls.Player1Right]))
+            {
+                deltaX = 1 * speed * Time.deltaTime;
+            }
+            else if (Input.GetKey(PlayerConstants.ControlsDict[PlayerConstants.PlayerControls.Player1Left]) &&
+                Input.GetKey(PlayerConstants.ControlsDict[PlayerConstants.PlayerControls.Player1Right]))
+            {
+                deltaX = 0;
+            }
+            if (Input.GetKey(PlayerConstants.ControlsDict[PlayerConstants.PlayerControls.Player1Up]))
+            {
+                deltaY = 1 * speed * Time.deltaTime;
+            }
+            else if (Input.GetKey(PlayerConstants.ControlsDict[PlayerConstants.PlayerControls.Player1Down]))
+            {
+                deltaY = -1 * speed * Time.deltaTime;
+            }
+            else if (Input.GetKey(PlayerConstants.ControlsDict[PlayerConstants.PlayerControls.Player1Up]) &&
+                Input.GetKey(PlayerConstants.ControlsDict[PlayerConstants.PlayerControls.Player1Down]))
+            {
+                deltaY = 0;
+            }
+        }
+        else if (playerNumber == 2)
+        {
+            if (Input.GetKey(PlayerConstants.ControlsDict[PlayerConstants.PlayerControls.Player2Left]))
+            {
+                deltaX = -1 * speed * Time.deltaTime;
+            }
+            else if (Input.GetKey(PlayerConstants.ControlsDict[PlayerConstants.PlayerControls.Player2Right]))
+            {
+                deltaX = 1 * speed * Time.deltaTime;
+            }
+            else if (Input.GetKey(PlayerConstants.ControlsDict[PlayerConstants.PlayerControls.Player2Left]) &&
+                Input.GetKey(PlayerConstants.ControlsDict[PlayerConstants.PlayerControls.Player2Right]))
+            {
+                deltaX = 0;
+            }
+            if (Input.GetKey(PlayerConstants.ControlsDict[PlayerConstants.PlayerControls.Player2Up]))
+            {
+                deltaY = 1 * speed * Time.deltaTime;
+            }
+            else if (Input.GetKey(PlayerConstants.ControlsDict[PlayerConstants.PlayerControls.Player2Down]))
+            {
+                deltaY = -1 * speed * Time.deltaTime;
+            }
+            else if (Input.GetKey(PlayerConstants.ControlsDict[PlayerConstants.PlayerControls.Player2Up]) &&
+                Input.GetKey(PlayerConstants.ControlsDict[PlayerConstants.PlayerControls.Player2Down]))
+            {
+                deltaY = 0;
+            }
+
+        }
+        controller.Move(new Vector3(deltaX, deltaY));
+        #endregion
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag.Equals(GameConstants.TrashTag))
+        {
+            Debug.Log(string.Format("{0} is near a trash can", name));
+        }
+    }
+}
---
+++ Assets/AssetLibrary/Scripts/Player/PlayerConstants.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using UnityEngine;
+
+class PlayerConstants
+{
+    public enum PlayerControls
+    {
+        Player1Left,
+        Player1Right,
+        Player1Up,
+        Player1Down,
+        Player1Interact,
+
+        Player2Left,
+        Player2Right,
+        Player2Up,
+        Player2Down,
+        Player2Interact,
+
+
+    }
+
+    public static readonly Dictionary<PlayerControls, KeyCode> ControlsDict = new Dictionary<PlayerControls, KeyCode>
+    {
+        { PlayerControls.Player1Left, KeyCode.A },
+        { PlayerControls.Player1Right, KeyCode.D },
+        { PlayerControls.Player1Up, KeyCode.W },
+        { PlayerControls.Player1Down, KeyCode.S },
+        { PlayerControls.Player1Interact, KeyCode.LeftShift },
+
+        { PlayerControls.Player2Left, KeyCode.LeftArrow },
+        { PlayerControls.Player2Right, KeyCode.RightArrow },
+        { PlayerControls.Player2Up, KeyCode.UpArrow },
+        { PlayerControls.Player2Down, KeyCode.DownArrow },
+        { PlayerControls.Player2Interact, KeyCode.RightControl },
+    };
+
+
+}
